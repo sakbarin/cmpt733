748d1df02623afd02f907469a356e83baed3a5ca
Shahram Akbarinasaji <sakbarin@sfu.ca>
2020-01-16 20:10:19 -0800

    implementing jacard similarity join.

---
+++ assignment-2/similarity_join.py
@@ -0,0 +1,53 @@
+# similarity_join.py
+import re
+import pandas as pd
+
+class SimilarityJoin:
+    def __init__(self, data_file1, data_file2):
+        self.df1 = pd.read_csv(data_file1)
+        self.df2 = pd.read_csv(data_file2)
+
+    def preprocess_df(self, df, cols): 
+        """
+            Write your code!
+        """ 
+
+    def filtering(self, df1, df2):
+        """
+            Write your code!
+        """
+
+    def verification(self, cand_df, threshold):
+        """
+            Write your code!
+        """
+
+    def evaluate(self, result, ground_truth):
+        """
+            Write your code!
+        """
+
+    def jaccard_join(self, cols1, cols2, threshold):
+        new_df1 = self.preprocess_df(self.df1, cols1)
+        new_df2 = self.preprocess_df(self.df2, cols2)
+        print ("Before filtering: %d pairs in total" %(self.df1.shape[0] *self.df2.shape[0])) 
+
+        cand_df = self.filtering(new_df1, new_df2)
+        print ("After Filtering: %d pairs left" %(cand_df.shape[0]))
+
+        result_df = self.verification(cand_df, threshold)
+        print ("After Verification: %d similar pairs" %(result_df.shape[0]))
+
+        return result_df
+
+
+
+if __name__ == "__main__":
+    er = SimilarityJoin("Amazon_sample.csv", "Google_sample.csv")
+    amazon_cols = ["title", "manufacturer"]
+    google_cols = ["name", "manufacturer"]
+    result_df = er.jaccard_join(amazon_cols, google_cols, 0.5)
+
+    result = result_df[['id1', 'id2']].values.tolist()
+    ground_truth = pd.read_csv("Amazon_Google_perfectMapping_sample.csv").values.tolist()
+    print ("(precision, recall, fmeasure) = ", er.evaluate(result, ground_truth))
