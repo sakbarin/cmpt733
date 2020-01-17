6b299971856c3a38ad80673f92948e117ce95c42
Shahram Akbarinasaji <sakbarin@sfu.ca>
2020-01-16 23:10:13 -0800

    implement preprocess data method.

--- assignment-2/similarity_join.py
+++ assignment-2/similarity_join.py
@@ -7,10 +7,27 @@ class SimilarityJoin:
         self.df1 = pd.read_csv(data_file1)
         self.df2 = pd.read_csv(data_file2)
 
-    def preprocess_df(self, df, cols): 
-        """
-            Write your code!
-        """ 
+    def concat_columns(self, cols):
+        # concat columns values with a space between
+        return ' '.join(cols.values.astype(str))
+    
+    def tokenize(self, col_value):
+        # tokenize input column value
+        values = re.split(r'\W+', col_value.strip())
+        return [value.lower() for value in values if (value.lower() != '' and value.lower() != 'nan')]
+    
+    def preprocess_df(self, df, cols):         
+        # concat provided columns in new column called concat_col        
+        df['concat_col'] = df[cols].apply(self.concat_columns, axis=1)
+        
+        # tokenize and lower concatenated columns and save it in joinKey column
+        df['joinKey'] = df['concat_col'].apply(self.tokenize)
+        
+        # drop previously concatenated column
+        df.drop('concat_col', inplace=True, axis=1)
+
+        # return result dataframe
+        return df
 
     def filtering(self, df1, df2):
         """
