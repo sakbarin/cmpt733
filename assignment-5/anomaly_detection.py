92f7536d936ee18bdd0f3877897ef3eed017fe0e
Shahram Akbarinasaji <sakbarin@sfu.ca>
2020-02-08 21:31:18 -0800

    implement functions for one-hot encoding, standardize numerical columns, and kmeans to find anomalies.

--- assignment-5/anomaly_detection.py
+++ assignment-5/anomaly_detection.py
@@ -1,61 +1,73 @@
 # anomaly_detection.py
 import pandas as pd
 import numpy as np
+import ast 
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
 class AnomalyDetection():
 
-    def normalizeDf(self, df):
-        for i in range(len(df['features'][0])):
-            df['f' + str(i + 1)] = df['features'].apply(lambda x: x[i])
-
-        df.drop(columns=['features'], inplace=True)
-        return df
-    
-    def scaleNum(self, df, indices):
-        # break feature list to some columns
-        df_normal = self.normalizeDf(df)
-
-        # add one to indices to (because of id column)
-        indices = np.add(indices, 1)
+    def scaleNum(self, df, indices):    
+        df_normal = pd.DataFrame(df['features'].tolist())
+        df_normal = df_normal.reset_index()
+        df_normal.rename(columns={'index': 'id'}, inplace=True)
         
+        # add 1 to indices [because of index column]
+        indices = np.add(indices, 1)
+
+        # iterate through all indices
         for index in indices:
+            
+            # find mean of column index
             mean = df_normal.iloc[:,index].mean()
 
+            # find x - mean
             df_normal['x_mean'] = df_normal.iloc[:,index] - mean
 
+            # find (x - mean) ^ 2
             df_normal['(x_mean)pow2'] = df_normal['x_mean'] * df_normal['x_mean']
 
+            # find variance
             variance = df_normal['(x_mean)pow2'].sum() / (df_normal['(x_mean)pow2'].count() - 1)
+            
+            # find std dev
             standard_deviation = np.sqrt(variance)
 
+            # set column value to (x - mean) / std dev
             df_normal.iloc[:,index] = df_normal['x_mean'] / standard_deviation
-
+            
+            # drop temp columns
             df_normal.drop(columns=['x_mean', '(x_mean)pow2'], inplace=True)
 
+        # replace all nan values to 0 [nan because of div by zero]
+        df_normal.fillna(0, inplace=True)
+        
+        # merge all values to feature column
         df_normal['features'] = df_normal.iloc[:,1:].to_numpy().tolist()
 
+        # return result
         return df_normal[['id', 'features']]
 
 
-    def cat2Num(self, df, indices):
-        # break feature list to some columns
-        df_normal = self.normalizeDf(df)
-
-        # add one to indices to (because of id column)
-        indices = np.add(indices, 1)
-
+    
+    def cat2Num(self, df, indices): 
+        df_normal = df.reset_index()
+        df_normal.rename(columns={'index': 'id'}, inplace=True)
+        
         # keep column names
         columns = df_normal.columns
 
         # no. of columns added
         new_added_columns = 0
         
+        # add 1 to indices [because of index column]
+        indices = np.add(indices, 1)
+        
         # iterate indicdes times
-        for index in indices:
-            
+        for index in indices:            
             # get all distinct values in column index
             df_items = df_normal.iloc[:,index].drop_duplicates().reset_index(drop=True)
-
+            
             # generate an identity matrix (with len(items) rows) for that column
             df_identity = pd.DataFrame(data=np.identity(len(df_items), dtype=int))
             
@@ -71,45 +83,74 @@ class AnomalyDetection():
 
             # join main input matrix with concatenated matrix to have one hot encoding as columns of that matrix
             df_normal = pd.merge(left=df_normal, right=df_concat, how='inner', on=columns[index])
-
+    
+    
         # drop input columns [because we have one-hot encoding columns instead]
         df_normal.drop(columns=df_normal.columns[indices], inplace=True)
         
         # list of all new columns
         final_columns = list(df_normal.columns)
-
+        
         # put columns in order [id, all generated one-hot encoding columns, rest of columns]
         df_normal = df_normal[final_columns[0:1] + final_columns[-1 * new_added_columns:] + final_columns[1:len(final_columns)-new_added_columns]]
-        
+    
         # create a feature column
         df_normal['features'] = df_normal.iloc[:,1:].to_numpy().tolist()
         
-        # return [id, feature] dataframe
         return df_normal[['id', 'features']].sort_values('id')
 
     def detect(self, df, k, t):
-        """
-            Write your code!
-        """
+        # init KMeans 
+        kmeans = KMeans(n_clusters=k)
+        
+        # convert features to lists
+        x_train = list(df['features'])      
+    
+        # fit training set
+        kmeans.fit(x_train)
 
-pd.set_option('display.max_colwidth', -1)
+        # copy predicted labels to dataframe
+        df_temp = df.copy()
+        df_temp['cluster'] = kmeans.labels_
 
-#df = pd.read_csv('logs-features-sample.csv').set_index('id')
+        # groupby clusters to get max, min, and each group count
+        df_count = df_temp[['features', 'cluster']].groupby('cluster', as_index=False).count()
+        df_count.rename(columns={'features': 'cluster_count'}, inplace=True)
+
+        # get max cluster count
+        n_max = df_count['cluster_count'].max()
+
+        # get min cluster count
+        n_min = df_count['cluster_count'].min()
+
+        # merge temp dataframe and count of clusters to have a column showing count of that row's cluster        
+        df_result = pd.merge(left=df_temp, right=df_count, on='cluster')
+
+        # calculate score
+        df_result['score'] = (n_max - df_result['cluster_count']) / (n_max - n_min)
+
+        # return required columns and filter rows having value greater than threshold
+        return df_result[['id', 'features', 'score']].loc[df_result['score'] >= t]
+        
+
+#pd.set_option('display.max_colwidth', -1)
+
+
 ad = AnomalyDetection()
 
-data = [(0, ["http", "udt", 4]), \
-        (1, ["http", "udf", 5]), \
-        (2, ["http", "tcp", 5]), \
-        (3, ["ftp", "icmp", 1]), \
-        (4, ["http", "tcp", 4])]
+file_name = 'logs-features-sample.csv'
+col_name = 'features'
 
-df = pd.DataFrame(data=data, columns = ["id", "features"])
+
+df = pd.read_csv(file_name, converters={col_name: ast.literal_eval})
+df = pd.DataFrame(df[col_name].tolist())
+
 
 df1 = ad.cat2Num(df, [0,1])
 print(df1)
 
-df2 = ad.scaleNum(df1, [6])
+df2 = ad.scaleNum(df1, list(range(12,48)))
 print(df2)
 
-#df3 = ad.detect(df2, 8, 0.97)
-#print(df3)
+df3 = ad.detect(df2, 8, 0.97)
+print(df3)
